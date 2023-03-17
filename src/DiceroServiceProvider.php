b5fdaaa288eb8a29607ce0dcc9f7473b1a6cb8f0
Bayu Ghara <54949300+iromashiro@users.noreply.github.com>
2023-03-18 04:06:30 +0700

    Update DiceroServiceProvider.php

--- src/DiceroServiceProvider.php
+++ src/DiceroServiceProvider.php
@@ -3,26 +3,30 @@
 namespace Diskominfo;
 
 use Illuminate\Support\ServiceProvider;
+use Diskominfo\Middlewares\AuthLogin;
+use Diskominfo\Middlewares\MustLogin;
+use Diskominfo\Middlewares\AdminLogin;
 
-class DiceroServiceProvider extends ServiceProvider {
+class DiceroServiceProvider extends ServiceProvider
+{
 
-    public function boot(){
+    public function boot()
+    {
 
         $this->publishes([
-            __DIR__.'/config/dicero.php' => config_path('dicero.php'),
-            __DIR__.'/database/migrations/' => database_path('migrations'),
-             __DIR__.'/database/seeds/' => database_path('seeds'),
-            __DIR__.'/helpers/' => app_path()
-        ],'all');
-        
+            __DIR__ . '/config/dicero.php' => config_path('dicero.php'),
+            __DIR__ . '/database/migrations/' => database_path('migrations'),
+            __DIR__ . '/database/seeds/' => database_path('seeds'),
+            __DIR__ . '/helpers/' => app_path()
+        ], 'all');
+
         $this->loadMiddleware();
     }
-    
+
     protected function loadMiddleware()
     {
         app('router')->aliasMiddleware('auth.login', AuthLogin::class);
         app('router')->aliasMiddleware('opd.login', MustLogin::class);
         app('router')->aliasMiddleware('admin.login', AdminLogin::class);
-       
     }
 }
