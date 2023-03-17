702aa13ddebf72903a18dcc155d898d67857ead7
Bayu Ghara <54949300+iromashiro@users.noreply.github.com>
2023-03-18 02:39:02 +0700

    simplify publish

--- src/DiceroServiceProvider.php
+++ src/DiceroServiceProvider.php
@@ -9,19 +9,10 @@ class DiceroServiceProvider extends ServiceProvider {
     public function boot(){
 
         $this->publishes([
-            __DIR__.'/config/dicero.php' => config_path('dicero.php')
-        ],'config');
-
-        $this->publishes([
-            __DIR__.'/database/migrations/' => database_path('migrations')
-        ], 'migrations');
-
-        $this->publishes([
-            __DIR__.'/database/seeds/' => database_path('seeds')
-        ], 'seeds');
-
-        $this->publishes([
+            __DIR__.'/config/dicero.php' => config_path('dicero.php'),
+            __DIR__.'/database/migrations/' => database_path('migrations'),
+             __DIR__.'/database/seeds/' => database_path('seeds'),
             __DIR__.'/helpers/' => app_path()
-        ],'helpers');
+        ],'all');
     }
 }
