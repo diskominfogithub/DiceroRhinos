aacb4d262b579f1f29b039485a54ffafa4f8bf71
Bayu Ghara <54949300+iromashiro@users.noreply.github.com>
2024-05-10 18:45:20 +0700

    Update DiceroServiceProvider.php

--- src/DiceroServiceProvider.php
+++ src/DiceroServiceProvider.php
@@ -15,10 +15,12 @@ class DiceroServiceProvider extends ServiceProvider
     {
 
         $this->publishes([
+            __DIR__ . '/AuthController.php' => app_path('Http/Controllers/AuthController.php'),
             __DIR__ . '/config/dicero.php' => config_path('dicero.php'),
             __DIR__ . '/database/migrations/' => database_path('migrations'),
             __DIR__ . '/database/seeds/' => database_path('seeds'),
-            __DIR__ . '/helpers/' => app_path()
+            __DIR__ . '/helpers/' => app_path(),
+            __DIR__ . '/routes.php' => base_path("routes/web.php")
         ], 'all');
 
         $this->loadMiddleware();
